9411f01bad24e75d7bbf4327ddd93f86317a107c
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2020-10-29 19:08:50 +0200

    Create variable for shuffled questions

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() { //what happens when we clink on Start button
   startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
   questionContainerElement.classList.remove('hide')
   setNextQuestion()
 }
 
 function setNextQuestion() { //what happens when we clink on Next button
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function selectAnswer() { //what happens when we choose an answer
